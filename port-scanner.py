fbe8ba4ff816cb2b4a387c9684550a7da34951dd
VL_PLAY <mr.bonie39@gmail.com>
2024-09-14 10:09:43 +0300

    v0.2

--- port-scanner.py
+++ port-scanner.py
@@ -1,4 +1,4 @@
-import socket, pyray as pr
+import socket, pyray as pr, netifaces, subprocess, os
 from raylib import colors
 from concurrent.futures import ThreadPoolExecutor
 
@@ -24,6 +24,22 @@ def scan_ports(host, start_port, end_port):
                 # print(f"Порт {port} закрыт.")
     return open_ports
 
+
+def get_ip4_addresses():
+    ip_list = []
+    for interface in netifaces.interfaces():
+        for link in netifaces.ifaddresses(interface)[netifaces.AF_INET]:
+            ip_list.append(link['addr'])
+    return ip_list
+
+def get_ip6_addresses():
+    ip_list = []
+    for interface in netifaces.interfaces():
+        for link in netifaces.ifaddresses(interface)[netifaces.AF_INET6]:
+            ip_list.append(link['addr'])
+    return ip_list
+
+
 def main():
     width = 1000
     height = 600
@@ -43,6 +59,7 @@ def main():
         
         pr.draw_text("Select option", 50,50,25,colors.BLACK)
         pr.draw_text("Result", 550,50,25,colors.BLACK)
+        pr.draw_text("Enter IP and ports", 50, 175, 15, colors.BLACK)
 
         pr.draw_rectangle_lines(525, 100, 450, 450, colors.BLACK)
 
@@ -50,23 +67,50 @@ def main():
         if pr.gui_button(
                     pr.Rectangle(50, 100, 100, 50), 
                     'Check your IP'):   
-                draw_text = "Your IP is: "+ IP
-
+                draw_text = "Your IP v4 is: \n" + str(get_ip4_addresses()) + "\n \n" + "Your IP v6 is:" + "\n" + str(get_ip6_addresses())
+                
         # Получение открытых портов собственного IP 
         if pr.gui_button(
                     pr.Rectangle(200, 100, 100, 50), 
-                    'Check ports (1000)'): 
+                    'Check main ports'): 
                 pr.begin_drawing()
-                pr.draw_text("Checking...", 550, 125, 10, colors.BLACK)
+                pr.draw_text("Checking open ports...", 550, 125, 10, colors.BLACK)
                 pr.clear_background(colors.WHITE)
                 pr.end_drawing()
                 pr.begin_drawing()
-                open_ports = scan_ports(IP, 1, 1000)
-                if len(open_ports) != 0:
-                    draw_text = "Open ports: " + str(open_ports)
+                draw_text = "Checking open ports... \n \n" 
+                for ip in get_ip4_addresses():  
+                    open_ports = scan_ports(ip, 1, 10000)
+                    if len(open_ports) != 0:
+                        draw_text += "Open ports in " + ip + ":" + "\n" + str(open_ports) + "\n \n"
+                    else:
+                        draw_text += 'All ports are closed in ' + ip
+
+
+        # Получение полной информации
+        if pr.gui_button(
+                    pr.Rectangle(350, 100, 100, 50), 
+                    'All info'):   
+                pr.begin_drawing()
+                pr.draw_text("Checking info this may take a while", 550, 125, 10, colors.BLACK)
+                pr.clear_background(colors.WHITE)
+                pr.end_drawing()
+                pr.begin_drawing()
+                draw_text = "All information: \nNetwork devicess info \n"
+                if os.name == 'nt':
+                    draw_text += subprocess.check_output("ipconfig" ).decode('utf-8')
                 else:
-                    draw_text = 'All ports are closed'
-                      
+                    draw_text += subprocess.check_output("ifconfig" ).decode('utf-8')
+
+                draw_text += "Checking open ports... \n" 
+                for ip in get_ip4_addresses():  
+                    open_ports = scan_ports(ip, 1, 49151)
+                    if len(open_ports) != 0:
+                        draw_text += "Open ports in " + ip + ":" + "\n" + str(open_ports) + "\n \n"
+                    else:
+                        draw_text += 'All ports are closed in ' + ip
+
+                
         pr.draw_text(draw_text, 550, 125, 10, colors.BLACK)
         pr.clear_background(colors.WHITE)
         pr.end_drawing()
