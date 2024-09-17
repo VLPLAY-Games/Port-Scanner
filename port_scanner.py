2c03c4bc7a3103d4242a71385afb069df41d6271
VL_PLAY <mr.bonie39@gmail.com>
2024-09-17 15:16:58 +0300

    Refactoring

--- port_scanner.py
+++ port_scanner.py
@@ -1,3 +1,4 @@
+""" Сканер портов """
 import subprocess
 import os
 import socket
@@ -8,14 +9,15 @@ import netifaces
 from raylib import colors
 from config import width, height, fps, app_name, version
 
-logging.basicConfig(filename='report.log', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
-
+logging.basicConfig(filename='report.log', format='%(asctime)s - %(levelname)s - %(message)s', \
+                    level=logging.INFO)
 
 class Ip:
+    """ Класс для работы с IP """
     def __init__(self):
         self.ipv4_list = []
         self.ipv6_list = []
-    
+
     # Получение IP v4
     def get_ip4_addresses(self):
         self.ipv4_list = []
@@ -26,7 +28,7 @@ class Ip:
             except:
                 pass
         return self.ipv4_list
-    
+
     # Получение IP v6
     def get_ip6_addresses(self):
         self.ipv6_list = []
@@ -37,12 +39,13 @@ class Ip:
             except Exception as e:
                 logging.error(str(e) + version)
         return self.ipv6_list
-    
+
     def get_all_ip(self):
         return "Your IP v4 is: \n" + str(self.get_ip4_addresses()) + \
                     "\n \nYour IP v6 is:\n" + str(self.get_ip6_addresses())
-        
+
 class Port:
+    """ Класс для работы с портами"""
     def __init__(self):
         self.open_ports = []
 
@@ -66,16 +69,18 @@ class Port:
 
 
 class Task():
+    """ Класс для работы с заданиями"""
     def __init__(self):
         self.name_task = ""
-    
+
 class Keyboard:
+    """ Класс для работы с клавиатурой"""
     def __init__(self):
         self.keys = []
-    
+
     def get_keys(self):
         return self.keys
-    
+
     def append_keys(self, var):
         self.keys.append(var)
 
@@ -85,6 +90,7 @@ class Keyboard:
 
 
 class App(Keyboard):
+    """ Основной класс приложения"""
     def __init__(self):
         self.version = version
         self.width = width
@@ -97,7 +103,7 @@ class App(Keyboard):
         self.task = ""
         self.first_port = 0
         self.end_port = 0
-    
+
     def init_app(self):
         pr.init_window(self.width, self.height, self.app_name)
         pr.set_target_fps(self.fps)
@@ -114,9 +120,10 @@ class App(Keyboard):
         pr.draw_text(app_name + " by VL_PLAY Games " + version, 725, 585, 12, colors.BLACK)
 
     def draw_terminal(self , keys):
-        pr.draw_text(self.draw_text + str(''.join(keys)) if self.terminal_active else self.draw_text, \
+        pr.draw_text(self.draw_text + str(''.join(keys)) if self.terminal_active \
+                     else self.draw_text, \
                      550, 125, 10, colors.BLACK)
-        
+
     def error_init(self, e):
         logging.critical(str(e) + version)
         pr.init_window(300, 200, "Port Scanner Critical Error")
@@ -127,9 +134,10 @@ class App(Keyboard):
             pr.draw_text("Critical Error", 75, 75, 25, colors.BLACK)
             pr.end_drawing()
             pr.begin_drawing()
-        
+
 
 class Button:
+    """ Класс для работы с кнопками"""
     def __init__(self):
         pass
 
@@ -170,7 +178,8 @@ def main():
                 for ip_l in ip.get_ip4_addresses():
                     open_ports = port.scan_ports(ip_l, 1, 10000)
                     if len(open_ports) != 0:
-                        app.draw_text += "Open ports in " + ip_l + ":" + "\n" + str(open_ports) + "\n \n"
+                        app.draw_text += "Open ports in " + ip_l + \
+                            ":" + "\n" + str(open_ports) + "\n \n"
                     else:
                         app.draw_text += 'All ports are closed in ' + ip_l
 
@@ -225,7 +234,8 @@ def main():
                 if app.task == "ip_ports_start":
                     app.draw_text += str(end_port) + '\nStarting task... \n'
                     open_ports = port.scan_ports(task_ip, first_port, end_port)
-                    app.draw_text += "Open ports in " + task_ip + ":" + "\n" + str(open_ports) + "\n"
+                    app.draw_text += "Open ports in " + task_ip + \
+                        ":" + "\n" + str(open_ports) + "\n"
                     app.task = ""
                 elif app.task == "all_info":
                     pr.begin_drawing()
@@ -243,7 +253,8 @@ def main():
                     for ip_l in ip.get_ip4_addresses():
                         open_ports = port.scan_ports(ip_l, 1, 49151)
                         if len(open_ports) != 0:
-                            app.draw_text += "Open ports in " + ip_l + ":" + "\n" + str(open_ports) + "\n \n"
+                            app.draw_text += "Open ports in " + ip_l + \
+                                ":" + "\n" + str(open_ports) + "\n \n"
                         else:
                             app.draw_text += 'All ports are closed in ' + ip_l
                     app.task = ""
@@ -261,7 +272,7 @@ def main():
     except Exception as e:
         app.error_init(e)
     logging.info("App is closed")
-        
+
 
 if __name__ == '__main__':
     main()
